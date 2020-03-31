0d9206c8602cdde2860dc5e262410994d9ada534
tzvikif <tzvikif@gmail.com>
2020-03-31 15:35:34 +0300

    add spiral image

--- script.m
+++ script.m
@@ -2,14 +2,27 @@
 %load image
 close all;
 clear;
-squares = imread('fourSquares.tif');
-stripes = imread('stripes.tiff');
-[r,c] = size(squares);
-stripes = rgb2gray(stripes(:,:,1:3));
-
+threshold = 50;
+%hiddenIm = imread('fourSquares.tif');
+%hiddenIm = imread('stripes.tiff');
+hiddenIm = imread('spiral.tiff');
+hiddenIm = rgb2gray(hiddenIm(:,:,1:3));
+hiddenIm((hiddenIm > 50)) = 255;
+hiddenIm((hiddenIm <= 50)) = 0;
+[r,c] = size(hiddenIm);
 %figure;imshow(redSquares);
-figure;imshow(squares);
-maskedIm = buildMaskedIm(squares,0);
+figure;imshow(hiddenIm);
+maskedIm = buildMaskedIm(hiddenIm,0);
 figure;imshow(maskedIm);
+testImage = maskedIm;
+testImage(:,:,2) = zeros(r,c);
+testImage(:,:,3) = zeros(r,c);
+
+figure;imshow(testImage);
+
 %d = colorDist(RedStripes,RedStripes);
 %%
+p = [200 100 100];
+f = 50;
+pp = project_point(p,f);    
+disp(pp);
